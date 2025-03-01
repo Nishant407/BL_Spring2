4750be700ea6b15364de24579bc70de89b4f5fa8
Nishant407 <nishant407.ns@gmail.com>
2025-03-01 19:10:24 +0530

    UC2

--- src/main/java/com/example/MySecondSpring/controller/HelloWebController.java
+++ src/main/java/com/example/MySecondSpring/controller/HelloWebController.java
@@ -12,4 +12,10 @@ public class HelloWebController {
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+    @GetMapping("/web")
+    public String hello(){
+        return "hello";
+    }
+
+
 }
